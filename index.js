bea53493ba967115d9d47b90e7cad1684ad985dc
Kevin Wither <kvn.wither@gmail.com>
2022-04-10 12:49:02 -0600

    commit tree c39e65c5be54fdd52471303980906a8f817cb8b3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Kevin",
+    streetAddress: "21 Jump Street",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...obj};
+    newEmployee[key] = value;
+    return newEmployee;
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
